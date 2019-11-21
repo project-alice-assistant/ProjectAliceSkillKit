a962032cc4125e20c0b71c23a1658f6a1bce476b
maxbachmann <ssh@maxbachmann.de>
2019-11-21 18:28:33 +0100

    add some more templates

--- AliceSK/create/create.py
+++ AliceSK/create/create.py
@@ -3,16 +3,236 @@ from __future__ import print_function, unicode_literals
 import shutil
 from pathlib import Path
 import click
+import os
 
 from PyInquirer import style_from_dict, Token, prompt, Validator, ValidationError
 import jinja2
 
+class Moduler:
+	def __init__(self):
+		self._modulePath = None
+		self._general = None
+		
 
-def renderTemplate(templateFile: str, **kwargs) -> str:
-	templateLoader = jinja2.FileSystemLoader(searchpath='./templates/')
-	templateEnv = jinja2.Environment(loader=templateLoader, autoescape=True)
-	template = templateEnv.get_template(templateFile)
-	return template.render(**kwargs)
+	def start(self):
+		print('\nHey welcome in this basic module creation tool!')
+		self.generalQuestions()
+		self.createDestinationFolder()
+		self.createInstallFile()
+		self.createDialogTemplates()
+		self.createTalks()
+		self.createReadme()
+		self.createWidgets
+	
+		print('----------------------------\n')
+		print('All done!')
+		print(f"You can now start creating your module. You will find the main class in {self._modulePath}/{self._general['moduleName']}.py")
+		print('\nRemember to edit the dialogTemplate/XYZ.json and remove the dummy data!!\n')
+		print('Thank you for creating for Project Alice')
+
+
+	def generalQuestions(self):
+		answers = prompt(FIRST_QUESTION, style=STYLE)
+
+		self._modulePath = Path.home() / 'ProjectAliceModuler' / answers['username'] / answers['moduleName']
+
+		while self._modulePath.exists():
+			questions = [
+				{
+					'type'   : 'confirm',
+					'name'   : 'delete',
+					'message': 'Seems like this module name already exists.\nDo you want to delete it locally?',
+					'default': False
+				},
+				{
+					'type'    : 'input',
+					'name'    : 'moduleName',
+					'message' : 'Ok, so chose another module name please',
+					'validate': NotEmpty,
+					'filter'  : lambda val: str(val).title().replace(' ', ''),
+					'when'    : lambda subAnswers: not subAnswers['delete']
+				}
+			]
+			subAnswers = prompt(questions, style=STYLE)
+			if subAnswers['delete']:
+				shutil.rmtree(path=self._modulePath)
+			else:
+				self._modulePath = Path.home() / 'ProjectAliceModuler' / answers['username'] / subAnswers['moduleName']
+				answers['moduleName'] = subAnswers['moduleName']
+
+		subAnswers = prompt(NEXT_QUESTION, style=STYLE)
+		self._general = {**answers, **subAnswers}
+
+
+	def createTemplateFile(self, outputPath: str, templateFile: str, **kwargs) -> str:
+		templateLoader = jinja2.FileSystemLoader(searchpath=os.path.join(os.path.dirname(__file__), 'templates'))
+		templateEnv = jinja2.Environment(loader=templateLoader, autoescape=True)
+		template = templateEnv.get_template(templateFile)
+		(self._modulePath / outputPath).write_text(template.render(**kwargs))
+
+
+	def createDirectories(self, directories: list):
+		for directory in directories:
+			(self._modulePath / directory).mkdir(parents=True, exist_ok=True)
+
+
+	def createFiles(self, files: list):
+		for file in files:
+			(self._modulePath / file).touch(exist_ok=True)
+
+
+	def createDestinationFolder(self):
+		print('\n----------------------------')
+		print('Creating destination folders')
+
+		self.createDirectories([
+			'dialogTemplate',
+			'talks'
+		])
+
+		print('Creating python class')
+		self.createTemplateFile(f"{self._general['moduleName']}.py", 'module.py.j2',
+			moduleName=self._general['moduleName'],
+			description=self._general['description'],
+			username=self._general['username']
+		)
+
+
+	def createInstallFile(self):
+		reqs = list()
+		while True:
+			questions = [
+				{
+					'type'   : 'confirm',
+					'name'   : 'requirements',
+					'message': 'Do you want to add python pip requirements?',
+					'default': False
+				},
+				{
+					'type'    : 'input',
+					'name'    : 'req',
+					'message' : 'Enter the pip requirement name or `stop` to cancel',
+					'validate': NotEmpty,
+					'when'    : lambda subAnswers: subAnswers['requirements']
+				}
+			]
+			subAnswers = prompt(questions, style=STYLE)
+			if not subAnswers['requirements'] or subAnswers['req'] == 'stop':
+				break
+			reqs.append(subAnswers['req'])
+
+		sysreqs = list()
+		while True:
+			questions = [
+				{
+					'type'   : 'confirm',
+					'name'   : 'sysrequirements',
+					'message': 'Do you want to add system requirements?',
+					'default': False
+				},
+				{
+					'type'    : 'input',
+					'name'    : 'sysreq',
+					'message' : 'Enter the requirement name or `stop` to cancel',
+					'validate': NotEmpty,
+					'when'    : lambda subAnswers: subAnswers['sysrequirements']
+				}
+			]
+			subAnswers = prompt(questions, style=STYLE)
+			if not subAnswers['sysrequirements'] or subAnswers['sysreq'] == 'stop':
+				break
+			sysreqs.append(subAnswers['sysreq'])
+
+		print('Creating install file')
+
+		self.createTemplateFile(f"{self._general['moduleName']}.install", 'install.j2',
+			moduleName=self._general['moduleName'],
+			description=self._general['description'],
+			username=self._general['username'],
+			langs=self._general['langs'],
+			pipRequirements=reqs,
+			systemRequirements=sysreqs
+		)
+
+
+	def createDialogTemplates(self):
+		print('Creating dialog template(s)')
+		for lang in self._general['langs']:
+			print(f'- {lang}')
+			self.createTemplateFile(f'dialogTemplate/{lang}.json', 'dialog.json.j2',
+				moduleName=self._general['moduleName'],
+				description=self._general['description'],
+				username=self._general['username']
+			)
+
+
+	def createTalks(self):
+		print('Creating talks')
+		for lang in self._general['langs']:
+			print(f'- {lang}')
+			self.createTemplateFile(f'talks/{lang}.json', 'talks.json.j2')
+
+
+	def createReadme(self):
+		print('Creating readme file')
+		self.createTemplateFile('README.md', 'README.md.j2',
+			moduleName=self._general['moduleName'],
+			description=self._general['description'],
+			username=self._general['username'],
+			langs=self._general['langs']
+		)
+
+
+	def createWidgets(self):
+		moduleWidgets = []
+		while True:
+			questions = [
+				{
+					'type'   : 'confirm',
+					'name'   : 'widgets',
+					'message': 'Are you planning on creating widgets for you module? Widgets are used on the\ninterface to display quick informations that your module can return' if not moduleWidgets else 'Any other widgets?',
+					'default': False
+				},
+				{
+					'type'    : 'input',
+					'name'    : 'widget',
+					'message' : 'Enter the name of the widget',
+					'validate': NotEmpty,
+					'when'    : lambda subAnswers: subAnswers['widgets']
+				}
+			]
+			subAnswers = prompt(questions, style=STYLE)
+			if not subAnswers['widgets'] or subAnswers['widget'] == 'stop':
+				break
+			moduleWidgets.append(subAnswers['widget'])
+
+		if not moduleWidgets:
+			return
+
+		print('Creating widgets base directories')
+		self.createDirectories([
+			'widgets/css',
+			'widgets/fonts',
+			'widgets/img',
+			'widgets/js',
+			'widgets/lang',
+			'widgets/templates'
+		])
+
+		self.createFiles([
+			'widgets/__init__.py',
+			'widgets/css/common.css',
+			'widgets/img/.gitkeep',
+			'widgets/fonts/.gitkeep'
+		])
+
+		for widget in moduleWidgets:
+			widget = str(widget).title().replace(' ', '')
+			self.createTemplateFile(f'widgets/css/{widget}.css', 'widget.css.j2', widgetName=widget)
+			self.createTemplateFile(f'widgets/js/{widget}.js', 'widget.js.j2')
+			#(modulePath / 'widgets' / 'lang' / f'{widget}.lang.json').write_text('{}')
+			self.createTemplateFile(f'widgets/templates/{widget}.html', 'widget.html.j2', widget=widget)
+			self.createTemplateFile(f'widgets/{widget}.py', 'widget.py.j2', widget=widget)
 
 
 STYLE = style_from_dict({
@@ -33,144 +253,7 @@ class NotEmpty(Validator):
 				cursor_position=len(document.text)
 			)
 
-PYTHON_CLASS = '''from core.base.model.Module import Module
-from core.dialog.model.DialogSession import DialogSession
-from core.util.Decorators import IntentHandler
-class {moduleName}(Module):
-	"""
-	Author: {username}
-	Description: {description}
-	"""
-	@IntentHandler('MyIntentName')
-	def testIntent(self, session: DialogSession, **_kwargs):
-		pass
 
-
-	@IntentHandler('MySecondIntentName')
-	def secondTestIntent(self, session: DialogSession, **_kwargs):
-		pass'''
-
-INSTALL_JSON = '''{{
-	"name": "{moduleName}",
-	"version": "0.0.1",
-	"author": "{username}",
-	"maintainers": [],
-	"desc": "{description}",
-	"aliceMinVersion": "1.0.0",
-	"pipRequirements": [{pipRequirements}],
-	"systemRequirements": [{systemRequirements}],
-	"conditions": {{
-		"lang": [{langs}]
-	}}
-}}'''
-
-DIALOG_TEMPLATE = '''{{
-    "module": "{moduleName}",
-    "icon": "time",
-    "description": "{description}",
-    "slotTypes": [
-        {{
-            "name": "DummySlot",
-            "matchingStrictness": null,
-            "automaticallyExtensible": false,
-            "useSynonyms": true,
-            "values": [
-                {{
-                    "value": "foo",
-                    "synonyms": ["bar"]
-                }}
-            ]
-        }}
-    ],
-    "intents": [
-        {{
-            "name": "Dummy",
-            "description": "REMOVE this dummy intent!",
-            "enabledByDefault": true,
-            "utterances": [
-                "dummy {{foo:=>DummySlotName}}",
-                "bar"
-            ],
-            "slots": [
-                {{
-                    "name": "DummySlotName",
-                    "description": "dummy",
-                    "required": false,
-                    "type": "DummySlot",
-                    "missingQuestion": ""
-                }}
-            ]
-        }}
-    ]
-}}'''
-
-TALKS = '''{
-	"dummy": {
-		"default": [
-			"foobar",
-			"barfoo"
-		],
-		"short": [
-			"foo",
-			"bar"
-		]
-	},
-}'''
-
-README = '''# {moduleName}
-### Download
-##### > WGET method
-```bash
-wget http://bit.ly/????????? -O ~/ProjectAlice/system/moduleInstallTickets/{moduleName}.install
-```
-##### > Alice CLI method
-```bash
-./alice add module {username} {moduleName}
-```
-### Description
-{description}
-- Version: 0.0.1
-- Author: {username}
-- Maintainers: N/A
-- Alice minimum version: 1.0.0
-- Conditions:
-{langs}
-- Pip requirements: N/A
-- System requirements: N/A
-'''
-
-WIDGET_CSS = '''.{widgetName} {{
-	width: 100%;
-	height: 100%;
-	background-color: white;
-	padding: 5px;
-	box-sizing: border-box;
-}}
-'''
-
-WIDGET_JS = '''$(function(){});'''
-
-WIDGET_TEMPLATE = '''<div class="{widget}" id="{widget}">
-	<div class="widgetIcons">
-		<div class="widgetIcon">
-			<i class="fas fa-calendar-alt"></i>
-		</div>
-		<div class="widgetIcon" id="widgetSettings_{widget}">
-			<i class="fas fa-cog"></i>
-		</div>
-	</div>
-	<div>
-		
-	</div>
-</div>'''
-
-WIDGET_CLASS = '''import sqlite3
-from core.base.model.Widget import Widget
-class {widget}(Widget):
-	SIZE = 'w_small'
-	OPTIONS = dict()
-	def __init__(self, data: sqlite3.Row):
-		super().__init__(data)'''
 
 FIRST_QUESTION = [
 	{
@@ -229,222 +312,11 @@ NEXT_QUESTION = [
 	}
 ]
 
-def createDestinationFolder(modulePath, answers):
-	print('\n----------------------------')
-	print('Creating destination folders')
-
-	(modulePath / 'dialogTemplate').mkdir(parents=True)
-	(modulePath / 'talks').mkdir(parents=True)
-
-	print('Creating python class')
-	Path(modulePath, answers['moduleName']).with_suffix('.py').write_text(
-		PYTHON_CLASS.format(
-			moduleName=answers['moduleName'],
-			description=answers['description'],
-			username=answers['username']
-		)
-	)
-
-def createInstallFile(modulePath, answers):
-	reqs = list()
-	while True:
-		questions = [
-			{
-				'type'   : 'confirm',
-				'name'   : 'requirements',
-				'message': 'Do you want to add python pip requirements?',
-				'default': False
-			},
-			{
-				'type'    : 'input',
-				'name'    : 'req',
-				'message' : 'Enter the pip requirement name or `stop` to cancel',
-				'validate': NotEmpty,
-				'when'    : lambda subAnswers: subAnswers['requirements']
-			}
-		]
-		subAnswers = prompt(questions, style=STYLE)
-		if not subAnswers['requirements'] or subAnswers['req'] == 'stop':
-			break
-		reqs.append(subAnswers['req'])
-
-	sysreqs = list()
-	while True:
-		questions = [
-			{
-				'type'   : 'confirm',
-				'name'   : 'sysrequirements',
-				'message': 'Do you want to add system requirements?',
-				'default': False
-			},
-			{
-				'type'    : 'input',
-				'name'    : 'sysreq',
-				'message' : 'Enter the requirement name or `stop` to cancel',
-				'validate': NotEmpty,
-				'when'    : lambda subAnswers: subAnswers['sysrequirements']
-			}
-		]
-		subAnswers = prompt(questions, style=STYLE)
-		if not subAnswers['sysrequirements'] or subAnswers['sysreq'] == 'stop':
-			break
-		sysreqs.append(subAnswers['sysreq'])
-
-	print('Creating install file')
-	langs = ','.join([f'\n\t\t\t"{lang}"' for lang in answers['langs']])
-	if langs:
-		langs += '\n\t\t'
-
-	pipRequirements = ','.join([f'\n\t\t"{req}"' for req in reqs])
-	if pipRequirements:
-		pipRequirements += '\n\t'
-
-	systemRequirements = ','.join([f'\n\t\t"{req}"' for req in sysreqs])
-	if systemRequirements:
-		systemRequirements += '\n\t'
-
-	Path(modulePath, answers['moduleName']).with_suffix('.install').write_text(
-		INSTALL_JSON.format(
-			moduleName=answers['moduleName'],
-			description=answers['description'],
-			username=answers['username'],
-			langs=langs,
-			pipRequirements=pipRequirements,
-			systemRequirements=systemRequirements
-		)
-	)
-
-def createDialogTemplates(modulePath, answers):
-	print('Creating dialog template(s)')
-	for lang in answers['langs']:
-		print(f'- {lang}')
-		Path(modulePath, 'dialogTemplate', lang).with_suffix('.json').write_text(
-			DIALOG_TEMPLATE.format(
-				moduleName=answers['moduleName'],
-				description=answers['description'],
-				username=answers['username']
-			)
-		)
-
-def createTalks(modulePath, answers):
-	print('Creating talks')
-	for lang in answers['langs']:
-		print(f'- {lang}')
-		Path(modulePath, 'talks', lang).with_suffix('.json').write_text(TALKS)
-
-def createReadme(modulePath, answers):
-	print('Creating readme file')
-	langs = ''
-	for lang in answers['langs']:
-		langs += f'  - {lang}\n'
-
-	Path(modulePath, 'README.md').write_text(
-		README.format(
-			moduleName=answers['moduleName'],
-			description=answers['description'],
-			username=answers['username'],
-			langs=langs.rstrip()
-		)
-	)
-
-def createWidgets(modulePath, answers):
-	moduleWidgets = []
-	while True:
-		questions = [
-			{
-				'type'   : 'confirm',
-				'name'   : 'widgets',
-				'message': 'Are you planning on creating widgets for you module? Widgets are used on the\ninterface to display quick informations that your module can return' if not moduleWidgets else 'Any other widgets?',
-				'default': False
-			},
-			{
-				'type'    : 'input',
-				'name'    : 'widget',
-				'message' : 'Enter the name of the widget',
-				'validate': NotEmpty,
-				'when'    : lambda subAnswers: subAnswers['widgets']
-			}
-		]
-		subAnswers = prompt(questions, style=STYLE)
-		if not subAnswers['widgets'] or subAnswers['widget'] == 'stop':
-			break
-		moduleWidgets.append(subAnswers['widget'])
-
-	if not moduleWidgets:
-		return
-	
-	print('Creating widgets base directories')
-	(modulePath / 'widgets' / 'css').mkdir(parents=True, exist_ok=True)
-	(modulePath / 'widgets' / 'fonts').mkdir(parents=True, exist_ok=True)
-	(modulePath / 'widgets' / 'img').mkdir(parents=True, exist_ok=True)
-	(modulePath / 'widgets' / 'js').mkdir(parents=True, exist_ok=True)
-	(modulePath / 'widgets' / 'lang').mkdir(parents=True, exist_ok=True)
-	(modulePath / 'widgets' / 'templates').mkdir(parents=True, exist_ok=True)
-
-	(modulePath / 'widgets' / '__init__.py').touch(exist_ok=True)
-	(modulePath / 'widgets' / 'css' / 'common.css').touch(exist_ok=True)
-	(modulePath / 'widgets' / 'img' / '.gitkeep').touch(exist_ok=True)
-	(modulePath / 'widgets' / 'fonts' / '.gitkeep').touch(exist_ok=True)
-
-	for widget in moduleWidgets:
-		widget = str(widget).title().replace(' ', '')
-		(modulePath / 'widgets' / 'css' / f'{widget}.css').write_text(WIDGET_CSS.format(widgetName=widget))
-		(modulePath / 'widgets' / 'js' / f'{widget}.js').write_text(WIDGET_JS)
-		(modulePath / 'widgets' / 'lang' / f'{widget}.lang.json').write_text('{}')
-		(modulePath / 'widgets' / 'templates' / f'{widget}.html').write_text(WIDGET_TEMPLATE.format(widget=widget))
-		(modulePath / 'widgets' / f'{widget}.py').write_text(WIDGET_CLASS.format(widget=widget))
-
 
 @click.command()
 def create():
 	"""
 	creates a new module
 	"""
+	Moduler().start()
 
-	print('\nHey welcome in this basic module creation tool!')
-
-	print(renderTemplate('widget.js.j2'))
-	return
-	answers = prompt(FIRST_QUESTION, style=STYLE)
-
-	modulePath = Path.home() / 'ProjectAliceModuler' / answers['username'] / answers['moduleName']
-
-	while modulePath.exists():
-		questions = [
-			{
-				'type'   : 'confirm',
-				'name'   : 'delete',
-				'message': 'Seems like this module name already exists.\nDo you want to delete it locally?',
-				'default': False
-			},
-			{
-				'type'    : 'input',
-				'name'    : 'moduleName',
-				'message' : 'Ok, so chose another module name please',
-				'validate': NotEmpty,
-				'filter'  : lambda val: str(val).title().replace(' ', ''),
-				'when'    : lambda subAnswers: not subAnswers['delete']
-			}
-		]
-		subAnswers = prompt(questions, style=STYLE)
-		if subAnswers['delete']:
-			shutil.rmtree(path=modulePath)
-		else:
-			modulePath = Path.home() / 'ProjectAliceModuler' / answers['username'] / subAnswers['moduleName']
-			answers['moduleName'] = subAnswers['moduleName']
-
-	subAnswers = prompt(NEXT_QUESTION, style=STYLE)
-	answers = {**answers, **subAnswers}
-
-	createDestinationFolder(modulePath, answers)
-	createInstallFile(modulePath, answers)
-	createDialogTemplates(modulePath, answers)
-	createTalks(modulePath, answers)
-	createReadme(modulePath, answers)
-	createWidgets(modulePath, answers)
-
-	print('----------------------------\n')
-	print('All done!')
-	print('You can now start creating your module. You will find the main class in {}/{}.py'.format(modulePath, answers['moduleName']))
-	print('\nRemember to edit the dialogTemplate/XYZ.json and remove the dummy data!!\n')
-	print('Thank you for creating for Project Alice')
